a6291dd8533f8b884adadec7808204ed792c0826
harinikeshr <hari.nikesh.r.cce@gmail.com>
2026-02-04 07:25:54 +0530

    Initial commit, removed the dependencies.

--- src/main/java/org/example/javafullstacktraining/Main.java
+++ src/main/java/org/example/javafullstacktraining/Main.java
@@ -3,6 +3,10 @@ package org.example.javafullstacktraining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+    1. Add hatoeas dependency in the project.
+
+ */
 
 @SpringBootApplication
 public class Main {
