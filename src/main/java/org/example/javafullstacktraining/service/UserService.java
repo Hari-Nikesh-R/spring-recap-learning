37403fd5421236e5767b52998e928f2ac9c26ef8
Hari Nikesh R <hari.nikesh.r.cce@gmail.com>
2026-02-13 11:42:21 +0530

    Removed unnecessary file.

--- src/main/java/org/example/javafullstacktraining/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.example.javafullstacktraining.service;
-
-import org.example.javafullstacktraining.model.User;
-
-public interface UserService {
-    String saveStaticUserData(User user);
-}
