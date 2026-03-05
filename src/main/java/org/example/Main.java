cd221c78f294f0d59b9e3edb028110eab3226aa1
harinikeshr <hari.nikesh.r.cce@gmail.com>
2026-03-05 13:53:17 +0530

    Initial commit for Circuit breaker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,6 @@ package org.example;
 // Refer dsa_missing_numers.md in document for explanation
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {0,5,3,2,1};
-        int n = arr.length;
-        int missingCase = 0;
-        for (int j : arr) {
-            missingCase += j;
-        }
-        System.out.println((n * (n+1))/2 - missingCase);
+
     }
 }
